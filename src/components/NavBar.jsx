edf38265927adedc2383191bf373649f296802d1
Xydra <theminecraftgurru@gmail.com>
2024-05-07 19:46:25 -0400

    Changed how the nav functions and looks to make it scale better at different sizes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,61 @@
 function NavBar({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs d-flex justify-content-end flex-fill">
-      <h1 className="me-auto">Xydra01</h1>
-      <li className="nav-item mx-4">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        <h1 className="navbar-brand me-auto">Xydra01</h1>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          About
-        </a>
-      </li>
-      <li className="nav-item mx-4">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={
-            currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item mx-4">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item mx-3">
+              <a
+                className={`nav-link ${
+                  currentPage === 'About' ? 'active' : ''
+                }`}
+                aria-current={currentPage === 'About' ? 'page' : undefined}
+                href="#about"
+                onClick={() => handlePageChange('About')}
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item mx-3">
+              <a
+                className={`nav-link ${
+                  currentPage === 'Portfolio' ? 'active' : ''
+                }`}
+                aria-current={currentPage === 'Portfolio' ? 'page' : undefined}
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item mx-3">
+              <a
+                className={`nav-link ${
+                  currentPage === 'Contact' ? 'active' : ''
+                }`}
+                aria-current={currentPage === 'Contact' ? 'page' : undefined}
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
