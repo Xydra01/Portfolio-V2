ceef780a14a33affcb06f6f3141d417efe85f853
Xydra <theminecraftgurru@gmail.com>
2024-05-07 00:21:53 -0400

    New stuff

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,16 +5,113 @@ export default function Portfolio() {
       <div className="card-container row row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
         {/* Adjust the number of columns based on the viewport size */}
         <div className="col mb-4">
-          <div className="card">
-            <div className="card-body">Test 1</div>
+          <div className="card bg-dark text-white">
+            <img
+              src="https://placehold.co/600x400"
+              className="card-img"
+              alt="..."
+            ></img>
+            <div className="card-img-overlay">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
           </div>
         </div>
         <div className="col mb-4">
-          <div className="card">
-            <div className="card-body">Test 2</div>
+          <div className="card bg-dark text-white">
+            <img
+              src="https://placehold.co/600x400"
+              className="card-img"
+              alt="..."
+            ></img>
+            <div className="card-img-overlay">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
           </div>
         </div>
-        {/* Add more cards as needed */}
+        <div className="col mb-4">
+          <div className="card bg-dark text-white">
+            <img
+              src="https://placehold.co/600x400"
+              className="card-img"
+              alt="..."
+            ></img>
+            <div className="card-img-overlay">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
+          </div>
+        </div>
+        <div className="col mb-4">
+          <div className="card bg-dark text-white">
+            <img
+              src="https://placehold.co/600x400"
+              className="card-img"
+              alt="..."
+            ></img>
+            <div className="card-img-overlay">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
+          </div>
+        </div>
+        <div className="col mb-4">
+          <div className="card bg-dark text-white">
+            <img
+              src="https://placehold.co/600x400"
+              className="card-img"
+              alt="..."
+            ></img>
+            <div className="card-img-overlay">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
+          </div>
+        </div>
+        <div className="col mb-4">
+          <div className="card bg-dark text-white">
+            <img
+              src="https://placehold.co/600x400"
+              className="card-img"
+              alt="..."
+            ></img>
+            <div className="card-img-overlay">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
